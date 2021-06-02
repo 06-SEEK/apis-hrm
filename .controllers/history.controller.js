55b832f753009a94db17f90347fc4930552c2dc7
Van Hiep <levanhiep.95.95@gmail.com>
2021-06-02 09:28:53 +0700

    refector

---
+++ .controllers/history.controller.js
@@ -0,0 +1,57 @@
+const moment = require('moment');
+const History = require('../models/history.model');
+
+module.exports = {
+  listHistory: async function (req, res, next) {
+    // let data = await History.findByLambda();
+    // res.json(resSuccess(data));
+  },
+
+  findById: async function (req, res) {
+    // let id = req.params.id;
+    // let data = await History.findByLambda({ _id: id });
+    // res.json(resSuccess(data[0]));
+  },
+
+  postCreate: async function (req, res, next) {
+    // try {
+    // 	let entity = {
+    // 		user_id: req.body.user_id,
+    // 		result: req.body.result,
+    // 		create_at: moment.now(),
+    // 		is_deleted: false,
+    // 	};
+    // 	let history = await History.createByLambda(entity);
+    // 	res.json(
+    // 		resSuccess({
+    // 			data: history,
+    // 		})
+    // 	);
+    // } catch (error) {
+    // 	data = {
+    // 		...error,
+    // 		message: error.message,
+    // 		detail: error.detail,
+    // 	};
+    // 	res.json(resFail(data));
+    // }
+  },
+
+  delete: async function (req, res) {
+    // 	try {
+    // 		let id = req.params.id;
+    // 		let entity = {
+    // 			is_deleted: true,
+    // 		};
+    // 		let result = await History.updateByLambda({ _id: id }, entity);
+    // 		res.json(resSuccess(result));
+    // 	} catch (error) {
+    // 		data = {
+    // 			...error,
+    // 			message: error.message,
+    // 			detail: error.detail,
+    // 		};
+    // 		res.json(resFail(data));
+    // 	}
+  },
+};
